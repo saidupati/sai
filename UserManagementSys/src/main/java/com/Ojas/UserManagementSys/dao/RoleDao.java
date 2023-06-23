dc3c43c5c2652dbe2486963fd5d99ab57858e3e8
saidupati <127293731+saidupati@users.noreply.github.com>
2023-06-23 12:50:23 +0530

    Update RoleDao.java

--- UserManagementSys/src/main/java/com/Ojas/UserManagementSys/dao/RoleDao.java
+++ UserManagementSys/src/main/java/com/Ojas/UserManagementSys/dao/RoleDao.java
@@ -4,7 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Ojas.UserManagementSys.model.Role;
-
+//siddhardh
+///hii
+//prasanna
+///sai
 @Repository
 public interface RoleDao extends CrudRepository<Role, Long> {
     Role findRoleByName(String name);
